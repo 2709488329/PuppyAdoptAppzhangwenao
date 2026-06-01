685655af09cea346531502a0186e2ceca19fc42a
2709488329 <2709488329@qq.com>
2026-06-01 21:28:17 +0800

    Add app/src/main/java/com/puppyadopt/ui/theme/Color.kt

---
+++ app/src/main/java/com/puppyadopt/ui/theme/Color.kt
@@ -0,0 +1,31 @@
+package com.puppyadopt.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Material You 粉色系
+val Pink10 = Color(0xFFFCE4EC)
+val Pink20 = Color(0xFFF8BBD0)
+val Pink30 = Color(0xFFF48FB1)
+val Pink40 = Color(0xFFEC407A)
+val Pink50 = Color(0xFFE91E63)
+val Pink80 = Color(0xFFAD1457)
+val Pink90 = Color(0xFF880E4F)
+
+// 深色
+val DarkPink = Color(0xFFC2185B)
+val DarkText = Color(0xFF4A2C2C)
+val LightText = Color(0xFF8D6E6E)
+
+// 辅助色
+val Gold = Color(0xFFFFB300)
+val GoldLight = Color(0xFFFFD54F)
+val Cyan = Color(0xFF26C6DA)
+val CyanLight = Color(0xFF80DEEA)
+val Green = Color(0xFF2E7D32)
+val GreenLight = Color(0xFFE8F5E9)
+val Blue = Color(0xFF90CAF9)
+
+// 卡片/背景
+val CardBackground = Color(0xFFFFFFFF)
+val PageBackground = Color(0xFFFCE4EC)
+val DividerColor = Color(0xFFF0D0D8)
